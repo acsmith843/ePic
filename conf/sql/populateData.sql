aa496706e20f43e96dc0198a26f619b74ddc384c
Adam C. Smith <adam.smith@sparcedge.com>
2013-06-12 09:13:35 -0400

    updating populate data sql script

--- conf/sql/populateData.sql
+++ conf/sql/populateData.sql
@@ -1,3 +1,10 @@
 INSERT INTO USERS VALUES (1, 'Bob', 'Barker', 'bob.barker@gmail.com', 'bigbobby1234', NOW(), NOW());
 INSERT INTO USERS VALUES (2, 'Blaine', 'West', 'blaine.west@gmail.com', 'blazer4u', NOW(), NOW());
 INSERT INTO USERS VALUES (3, 'LA', 'Scalley', 'la.scalley@gmail.com', 'scalleywag69', NOW(), NOW());
+
+INSERT INTO ALBUMS VALUES (1, 'Tickle Party', '1', NOW(), NOW());
+INSERT INTO ALBUMS VALUES (2, 'Xmas', '1', NOW(), NOW());
+INSERT INTO ALBUMS VALUES (3, 'Easter', '2', NOW(), NOW());
+INSERT INTO ALBUMS VALUES (4, 'Memorial Day', '2', NOW(), NOW());
+INSERT INTO ALBUMS VALUES (5, 'New Years', '3', NOW(), NOW());
+INSERT INTO ALBUMS VALUES (6, '4th of July', '3', NOW(), NOW());
